21fa672aa2c62791953aa92aef1180e3bd97dc32
king-mahid-bot-wp <kingmahidmine@gmail.com>
2026-06-24 16:13:35 +0600

    Add files via upload

---
+++ Mahid/lib/groupConnected.js
@@ -0,0 +1,26 @@
+module.exports = function () {
+  return `${global.config.botName} CONNECTED«
+
+Assalamualaykum☘️
+<------------------------------>
+BOT CONNECTED SUCCESSFUL !!!
+
+APPROVAL ALLOW IN THIS GROUP!!!
+<------------------------------>
+
+TO VIEW ANY COMMAND Use👉 ${global.config.PREFIX}help
+See command👉 ${global.config.PREFIX}menu
+
+EXAMPLE:👇
+${global.config.PREFIX}admin (information)
+${global.config.PREFIX}info (admin info)
+${global.config.PREFIX}alldown (link)
+
+<------------------------------>
+
+AND FOR ANY COMPLAINTS OR CONTACT BOT OPERATOR 
+
+DEVELOPER: ${global.config.botOwner}
+
+🔵WHATSAPP NUMBER: wa.me/+${global.config.admin[0] || "8801864498984"}`;
+};
